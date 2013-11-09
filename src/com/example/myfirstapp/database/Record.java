cbca86452d32c0a0c073448f9c5a4b4ee78ebcf3
Nicolai Berthelsen <nicobert@cisco.com>
2013-11-09 14:51:53 +0100

    added class for record, which will be the object holding the song and artist

---
+++ src/com/example/myfirstapp/database/Record.java
@@ -0,0 +1,37 @@
+package com.example.myfirstapp.database;
+
+public class Record {
+  private long id;
+  private String artist;
+  private String song;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getArtist() {
+    return artist;
+  }
+  
+  public String getSong() {
+    return song;
+  }
+
+  public void setArtist(String artist) {
+    this.artist = artist;
+  }
+
+  public void setSong(String song) {
+    this.song = song;
+  }
+
+  // Will be used by the ArrayAdapter in the ListView
+  @Override
+  public String toString() {
+    return String.format("%s - %s", artist, song);
+  }
+} 
