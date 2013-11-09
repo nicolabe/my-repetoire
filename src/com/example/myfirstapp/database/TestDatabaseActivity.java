7224ae0776740a4b545a65c1813ed65b69e81aa6
Nicolai Berthelsen <nicobert@cisco.com>
2013-11-09 14:39:10 +0100

    initial attempt at getting a db up and running

--- src/com/example/myfirstapp/database/TestDatabaseActivity.java
+++ src/com/example/myfirstapp/database/TestDatabaseActivity.java
@@ -1,25 +1,27 @@
-package com.example.myfirstapp;
+package com.example.myfirstapp.database;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-public class MySQLiteHelper extends SQLiteOpenHelper {
-	public static final String TABLE_SONGS = "songs";
-	  public static final String SONG_ID = "_id";
+public class TestDatabaseActivity extends SQLiteOpenHelper {
+	  public static final String TABLE_RECORDS = "records";
+	  public static final String COLUMN_ID = "_id";
 	  public static final String COLUMN_SONG = "song";
+	  public static final String COLUMN_ARTIST = "artist";
 
-	  private static final String DATABASE_NAME = "commments.db";
+	  private static final String DATABASE_NAME = "records.db";
 	  private static final int DATABASE_VERSION = 1;
 
 	  // Database creation sql statement
-	  private static final String DATABASE_CREATE = "create table " + TABLE_SONGS + 
-	  "(" + COLUMN_ID
-	      + " integer primary key autoincrement, " + COLUMN_COMMENT
-	      + " text not null);";
+	  private static final String DATABASE_CREATE = "create table " 
+	  	   + TABLE_RECORDS + "(" + COLUMN_ID
+	       + " integer primary key autoincrement, "
+	       + COLUMN_SONG + " text not null, " 
+	       + COLUMN_ARTIST + " text not null);";
 
-	  public MySQLiteHelper(Context context) {
+	  public TestDatabaseActivity(Context context) {
 	    super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	  }
 
@@ -30,12 +32,11 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 
 	  @Override
 	  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-	    Log.w(MySQLiteHelper.class.getName(),
+	    Log.w(TestDatabaseActivity.class.getName(),
 	        "Upgrading database from version " + oldVersion + " to "
 	            + newVersion + ", which will destroy all old data");
-	    db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+	    db.execSQL("DROP TABLE IF EXISTS " + TABLE_RECORDS);
 	    onCreate(db);
 	  }
 
-	} 
 }
